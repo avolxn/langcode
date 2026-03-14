48fbb54ee5d238954bbb9723975db1b18e00af05
avolxn <vladislav.eldashov@gmail.com>
2026-03-14 19:31:01 +0300

    refactor: simplify globals.py by inlining XDG paths

--- src/langcode/config/globals.py
+++ src/langcode/config/globals.py
@@ -4,26 +4,6 @@ import os
 from pathlib import Path
 
 
-def _xdg_data_home() -> str:
-    """Get XDG data directory."""
-    return os.environ.get("XDG_DATA_HOME", str(Path.home() / ".local" / "share"))
-
-
-def _xdg_cache_home() -> str:
-    """Get XDG cache directory."""
-    return os.environ.get("XDG_CACHE_HOME", str(Path.home() / ".cache"))
-
-
-def _xdg_config_home() -> str:
-    """Get XDG config directory."""
-    return os.environ.get("XDG_CONFIG_HOME", str(Path.home() / ".config"))
-
-
-def _xdg_state_home() -> str:
-    """Get XDG state directory."""
-    return os.environ.get("XDG_STATE_HOME", str(Path.home() / ".local" / "state"))
-
-
 APP = "langcode"
 
 
@@ -38,32 +18,36 @@ class _GlobalPath:
     @property
     def data(self) -> str:
         """Data directory."""
-        return str(Path(_xdg_data_home()) / APP)
+        xdg_data = os.environ.get("XDG_DATA_HOME", str(Path.home() / ".local" / "share"))
+        return str(Path(xdg_data) / APP)
 
     @property
     def bin(self) -> str:
         """Binary directory."""
-        return str(Path(_xdg_data_home()) / APP / "bin")
+        return str(Path(self.data) / "bin")
 
     @property
     def log(self) -> str:
         """Log directory."""
-        return str(Path(_xdg_data_home()) / APP / "log")
+        return str(Path(self.data) / "log")
 
     @property
     def cache(self) -> str:
         """Cache directory."""
-        return str(Path(_xdg_cache_home()) / APP)
+        xdg_cache = os.environ.get("XDG_CACHE_HOME", str(Path.home() / ".cache"))
+        return str(Path(xdg_cache) / APP)
 
     @property
     def config(self) -> str:
         """Config directory."""
-        return str(Path(_xdg_config_home()) / APP)
+        xdg_config = os.environ.get("XDG_CONFIG_HOME", str(Path.home() / ".config"))
+        return str(Path(xdg_config) / APP)
 
     @property
     def state(self) -> str:
         """State directory."""
-        return str(Path(_xdg_state_home()) / APP)
+        xdg_state = os.environ.get("XDG_STATE_HOME", str(Path.home() / ".local" / "state"))
+        return str(Path(xdg_state) / APP)
 
     def ensure_dirs(self) -> None:
         """Ensure all directories exist."""
